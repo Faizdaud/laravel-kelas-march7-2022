c6311f142f689d669dd6af31c40e16c4fd055436
Faizdaud <ahmadfaizdaud007@gmail.com>
2022-03-08 10:54:57 +0800

    first commit

---
+++ resources/views/goodbye.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>My First Website</title>
+    </head>
+    <body>
+        <h1>Goodbye</h1>
+        {{-- get parameters from url (in this case $name) --}}
+        <p>Goodbye {{$name}}</p>
+    </body>
+</html>
