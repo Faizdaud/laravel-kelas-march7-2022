8f33a52277a7a38ee497715ed77530673ec45df6
Faizdaud <ahmadfaizdaud007@gmail.com>
2022-03-09 12:32:11 +0800

    add CRUD

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -39,9 +39,31 @@ class MovieController extends Controller
 
     public function update(Request $request, $id){
         
+        if ($movie){
+            //this line requires you to add fillable property in Movie model
+            $updated = $movie->fill($request->all())->save();
+
+            if($updated){
+                return response()->json(["success"=>true,"data"=>$movie]);
+            }else{
+                return response()->json(["success"=>false,"message"=>"Cannot Save"]);
+            }
+        }else{
+            return response()->json(["success"=>false, "message"=>"Movie not found"]);
+        }
     }
 
     public function delete(Request $request, $id){
-        
+        $movie = Movie::find($id);
+
+        if($movie){
+            if($movie->delete()){
+                return response()->json(["success"=>true, "message"=>"Delete Success"]);
+            }else{
+                return response()->json(["success"=>false, "message"=>"Delete Failed"]);
+            }
+        }else{
+            return response()->json(["success"=>false, "message"=>"Movie not found"]);
+        }
     }
 }
